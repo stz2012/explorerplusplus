21b76bdf1cae4d2139116de546feb6a62ed578b8
STZ2012 <tslroom@hotmail.com>
2013-12-06 11:39:32 +0900

    ビルドバッチの修正

--- Build/compile_with_env.bat
+++ Build/compile_with_env.bat
@@ -1,15 +1,48 @@
 @echo off
-set BOOST=C:\tmp\boost_1_54_0
-set PANTHEIOS=C:\tmp\pantheios-1.0.1
-set STLSOFT=C:\tmp\stlsoft-1.9.117
-set LIB=C:\tmp\boost_1_54_0\lib32-msvc-10.0
-call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
-devenv /useenv /build release ..\Explorer++\Explorer++.sln
-IF EXIST "..\Explorer++\Explorer++\Release\Explorer++.exe" (
-    copy /Y "..\Explorer++\Explorer++\Release\Explorer++.exe" .
+
+call :setBaseDir %~dp0..\..\
+set BOOST=%BASE_DIR%boost_1_54_0
+set BOOST_LIB=%BASE_DIR%boost_1_54_0\lib32-msvc-10.0
+set PANTHEIOS=%BASE_DIR%pantheios-1.0.1
+set STLSOFT=%BASE_DIR%stlsoft-1.9.117
+set TARGET_MODE=Release
+set TARGET_ARCH=x86
+set TARGET_LANG=JA
+
+IF DEFINED VS100COMNTOOLS (
+	rem Load VisualStudio Environment Variable
+	IF "%TARGET_ARCH%" == "x64" (
+		call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" amd64
+	) else (
+		call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" %TARGET_ARCH%
+	)
+) else (
+	echo VisualStudio 2010 is not installed.
+	goto End
 )
-devenv /useenv /build release ..\Translations\Explorer++JA\Explorer++JA.sln
-IF EXIST "..\Translations\Explorer++JA\Release\Explorer++JA.dll" (
-    copy /Y "..\Translations\Explorer++JA\Release\Explorer++JA.dll" .
+
+call :setBaseDir %~dp0..\
+call :buildSolution "%BASE_DIR%Explorer++\Explorer++.sln" Explorer++.exe
+call :buildSolution "%BASE_DIR%Translations\Explorer++%TARGET_LANG%\Explorer++%TARGET_LANG%.sln" Explorer++%TARGET_LANG%.dll
+goto End
+
+:setBaseDir
+set BASE_DIR=%~f1
+exit /b
+
+:buildSolution
+devenv /useenv /build %TARGET_MODE% %1
+call :setOutputDir %1
+IF EXIST "%OUTPUT_DIR%%2" (
+	mkdir %TARGET_ARCH% > NUL 2>&1
+	if ERRORLEVEL 1 cmd /c exit 0
+	copy /Y "%OUTPUT_DIR%%2" %TARGET_ARCH%\
 )
+exit /b
+
+:setOutputDir
+set OUTPUT_DIR=%~dp1%TARGET_MODE%\
+exit /b
+
+:End
 pause
--- Build/pantheios_compile_with_env.bat
+++ Build/pantheios_compile_with_env.bat
@@ -1,7 +1,33 @@
 @echo off
-set PANTHEIOS_ROOT=C:\tmp\pantheios-1.0.1
-set STLSOFT=C:\tmp\stlsoft-1.9.117
-cd %PANTHEIOS_ROOT%\build\vc10
-call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" x86
-nmake build
+
+call :setBaseDir %~dp0..\..\
+set PANTHEIOS_ROOT=%BASE_DIR%pantheios-1.0.1
+set STLSOFT=%BASE_DIR%stlsoft-1.9.117
+set TARGET_ARCH=x86
+
+IF DEFINED VS100COMNTOOLS (
+	rem Load VisualStudio Environment Variable
+	IF "%TARGET_ARCH%" == "x64" (
+		call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" amd64
+	) else (
+		call "%VS100COMNTOOLS%..\..\VC\vcvarsall.bat" %TARGET_ARCH%
+	)
+) else (
+	echo VisualStudio 2010 is not installed.
+	goto End
+)
+
+IF "%TARGET_ARCH%" == "x64" (
+	cd %PANTHEIOS_ROOT%\build\vc10.x64
+) else (
+	cd %PANTHEIOS_ROOT%\build\vc10
+)
+nmake build.libs
+goto End
+
+:setBaseDir
+set BASE_DIR=%~f1
+exit /b
+
+:End
 pause
